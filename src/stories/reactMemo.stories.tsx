e12febd3e9fc112226b4096e50e07216437680f3
ellijahcuts <ellijahcuts@gmail.com>
2021-08-13 00:28:22 +0300

    17 useMemo

--- src/stories/reactMemo.stories.tsx
+++ src/stories/reactMemo.stories.tsx
@@ -1,35 +1,78 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 export default {
-    title: 'ts_learning/React.memo.demo',
+    title: 'ts_learning/useMemo.demo',
 
 }
 
-const NewMessagesCounter = (props: { count: number }) => {
-    return <div>{props.count}</div>
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 1000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            resultA = resultA * i
+        }
+        return resultA
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i
+    }
+
+    return <>
+        <input value={a} onChange={(e => setA(+e.currentTarget.value))}/>
+        <input value={b} onChange={(e => setB(+e.currentTarget.value))}/>
+        <hr/>
+        <div>
+            result for a:{resultA}
+        </div>
+        <div>
+            result for b:{resultB}
+        </div>
+    </>
+
 }
+
+
 const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("UsersSecret")
     return <div>
         {props.users.map((u, i) => <div>{u}</div>)}
     </div>
 }
 const Users = React.memo(UsersSecret)
 
-export const Example1 = () => {
-    console.log('Example1')
+export const HelpsForMemoExample = () => {
+    console.log("HelpsForMemoExample")
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(["Dima", "Misha", "Val9"])
 
+    const newArray = useMemo(() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf('d') > -1)
+        return newArray
+    }, [users])
+
     const addUser = () => {
-        const newUser = [...users,"Max " + new Date().getTime()]
+        const newUser = [...users,"MadMax " + new Date().getTime()]
         setUsers(newUser)
     }
-
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
         <button onClick={addUser}>add user</button>
-        <NewMessagesCounter count={counter}/>
-        <Users users={users}/>
+        <Users users={newArray}/>
     </>
 
 }
+
+
