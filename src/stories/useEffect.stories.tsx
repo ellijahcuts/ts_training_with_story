75447d086b46fc5ccb71877a6042c55acfd3cc93
ellijahcuts <ellijahcuts@gmail.com>
2021-08-17 19:26:48 +0300

    24 useEffect cleanup (сброс, зачистка)

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -22,3 +22,62 @@ export const SetTimeOutEx = () => {
 
     </>
 }
+export const ResetEffectEx = () => {
+    const [counter, setCounter] = useState(1)
+    console.log("Rendered")
+
+    useEffect(() => {
+        console.log("Effect on " + counter)
+
+        return ()=>{
+            console.log("Reset Effect")
+        }
+    },[counter])
+
+    const inc = () => {setCounter(counter+1)}
+
+    return <>
+        Hello mother fucker, Counter:{counter}
+        <button onClick={inc}>+++</button>
+    </>
+}
+export const KeyTrackerEx = () => {
+    const [text, setText] = useState('')
+    console.log("Rendered "+ text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent)=>{
+            console.log(e.key)
+            setText(text + e.key)
+        }
+        window.addEventListener('keypress',handler)
+        return()=>{
+            window.removeEventListener('keypress',handler)
+        }
+    },[text])
+
+
+    return <>
+        Typed text: {text}
+    </>
+}
+
+export const TimeoutEx = () => {
+    const [text, setText] = useState('')
+    console.log("Rendered "+ text)
+
+    useEffect(() => {
+        const timeoutID = setTimeout(()=>{
+            console.log('ppc')
+            setText('3sec proshlo')
+        },3000)
+        return () =>{
+clearTimeout(timeoutID)
+        }
+    },[text])
+
+
+    return <>
+        Typed text: {text}
+    </>
+}
